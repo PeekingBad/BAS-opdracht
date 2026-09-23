e3247f4cfb9d08e1bf5ebf480319cad8f4d3fdac
Verwegen,Daniell,D. <danielverwegen@live.nl>
2026-09-23 18:39:39 +0200

    feat: add parts list endpoint with mock data & delete tailwind test text

---
+++ server/api/parts/index.get.ts
@@ -0,0 +1,5 @@
+import { parts } from '#server/data/mock-parts'
+
+export default defineEventHandler(() => {
+  return parts
+})
---
+++ server/data/mock-parts.ts
@@ -0,0 +1,35 @@
+export const parts: Part[] = [
+    {
+      "id": "2119711",
+      "name": "DAF XF 106 Sideskirt set",
+      "oem": "2119711",
+      "brand": "DAF",
+      "condition": "used",
+      "price": 795,
+      "inStock": true,
+      "category": "Body",
+      "image": "/img/placeholder-part.png"
+    },
+    {
+      "id": "1948743",
+      "name": "DAF XF 106 Headlight left",
+      "oem": "1948743",
+      "brand": "DAF",
+      "condition": "new",
+      "price": 412,
+      "inStock": true,
+      "category": "Lighting",
+      "image": "/img/placeholder-part.png"
+    },
+    {
+      "id": "2006354",
+      "name": "DAF CF Fuel tank 430L",
+      "oem": "2006354",
+      "brand": "DAF",
+      "condition": "refurbished",
+      "price": 1150,
+      "inStock": false,
+      "category": "Chassis",
+      "image": "/img/placeholder-part.png"
+    },
+  ]
