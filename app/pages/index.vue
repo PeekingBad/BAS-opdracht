489231db456ac3665d962663ea45efa876de8efa
Verwegen,Daniell,D. <danielverwegen@live.nl>
2026-09-24 14:11:02 +0200

    feat: fetch and test list of parts on index page

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const { data: parts } = await useFetch('/api/parts')
+</script>
+
+<template>
+<main>
+  <h1>Welcome to the Parts Store</h1>
+  <ul>
+    <li v-for="part in parts" :key="part.id">
+      <h2>{{ part.name }}</h2>
+      <p>€{{ part.price }}</p>
+      <img :src="part.image" :alt="part.name" />
+    </li>
+  </ul>  
+</main>
+</template>
